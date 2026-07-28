f5346c0827a15991d545931d821ed6e2b9895d10
HaryKali <HaryKali@outlook.com>
2026-07-28 21:53:29 +1000

    tiny update for scope

--- lab_code/llvm_variable/scope.cc
+++ lab_code/llvm_variable/scope.cc
@@ -2,20 +2,41 @@
 
 Scope::Scope()
 {
-    envs.push_back(std::make_shared<Env>()); //global
+    envs.push_back(std::make_shared<Env>()); // global env
 }
 void Scope::EnterScope()
 {
+    envs.push_back(std::make_shared<Env>());
 }
 void Scope::ExitScope()
 {
+    if (envs.size() > 1)
+        envs.pop_back();
 }
 std::shared_ptr<Symbol> Scope::FindSymbol(llvm::StringRef name)
 {
+    for (auto it = envs.rbegin(); it != envs.rend(); ++it)
+    {
+        auto &table = (*it)->VariableSymbolTable;
+        if (table.count(name) > 0)
+        {
+            return table[name];
+        }
+    }
+    return nullptr;
 }
+
 std::shared_ptr<Symbol> Scope::FindSymbolInCurrentScope(llvm::StringRef name)
 {
+    auto &table = envs.back()->VariableSymbolTable;
+    if (table.count(name) > 0)
+    {
+        return table[name];
+    }
+    return nullptr;
 }
 void Scope::AddSymbol(SymbolKind kind, CType *ty, llvm::StringRef name)
 {
+    auto symbol = std::make_shared<Symbol>(kind, ty, name);
+    envs.back()->VariableSymbolTable.insert({name, symbol});
 }
